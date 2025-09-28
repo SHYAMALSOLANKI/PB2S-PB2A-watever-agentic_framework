2b4c299d1f360e48fc7b457102dc919b768a03e3
SHYAMALSOLANKI <SOLANKISHYAMAL5@GMAIL.COM>
2025-09-28 20:19:02 +0200

    Add files via upload

---
+++ api_connectors.py
@@ -0,0 +1,3 @@
+# Integration port for dynamic API connectors.
+# All attachments validated through PB2S schema.
+pass
---
+++ plugins.py
@@ -0,0 +1,3 @@
+# Integration port for plugins.
+# All attachments validated through PB2S schema.
+pass
